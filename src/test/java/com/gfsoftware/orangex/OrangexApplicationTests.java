f07fab7ea5267f2768221a1f666e7ef2493a081d
Gleison Fernandes da Silva <gleison.fernandesb@gmail.com>
2024-06-04 15:02:58 -0300

    :tada: Commit inicial

---
+++ src/test/java/com/gfsoftware/orangex/OrangexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfsoftware.orangex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
